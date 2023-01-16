476ec8465843c7c10dcf07249976108ff9170484
Tyler Carver <tbcarver@yahoo.com>
2023-01-15 21:36:09 -0700

    Fix for copied jwt strategy.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,7 +6,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/user/user.entity';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { JwtStrategy } from './jwt.strategy';
+import { JwtStrategy } from './jwt/jwt.strategy';
 
 @Module({
   imports: [
